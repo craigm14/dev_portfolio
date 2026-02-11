c40622b915d59dfdac3332fbec9d7a21447d30e2
craigm14 <craig-moore22@outlook.com>
2026-02-11 16:22:35 +0000

    Day 6 - small banner updates

--- dev_portfolio/src/conponents/Hero/Banner.jsx
+++ dev_portfolio/src/conponents/Hero/Banner.jsx
@@ -1,15 +1,15 @@
 import heroimg from "../../assets/profile.jpg";
 function Banner() {
   return (
-    <section class="max-w-[700px] mx-auto px-14 py-6">
-      <div class="flex items-start justify-between gap-6">
-        <div class="flex items-start gap-4">
+    <section className="max-w-[700px] mx-auto pt-10">
+      <div className="flex items-start justify-between gap-6">
+        <div className="flex items-start gap-4">
           <img
-            class="w-30 h-32 rounded-2xl"
+            className="w-30 h-32 rounded-2xl"
             src={heroimg}
             alt="Profile picture - Craig"
           />
-          <div class="intro">
+          <div className="intro">
             <h1>Craig Moore</h1>
             <h2>Website coordinator</h2>
             <h3>Glasgow, UK</h3>
@@ -21,27 +21,35 @@ function Banner() {
             </span>
           </div>
         </div>
-        <div class="flex self-end">
-          <a href="" class="align-self flex-end">
+        <div className="flex self-end">
+          <a href="" className="align-self flex-end">
             Download CV
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M9 8.25H7.5a2.25 2.25 0 0 0-2.25 2.25v9a2.25 2.25 0 0 0 2.25 2.25h9a2.25 2.25 0 0 0 2.25-2.25v-9a2.25 2.25 0 0 0-2.25-2.25H15M9 12l3 3m0 0 3-3m-3 3V2.25"
+              />
+            </svg>
           </a>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="size-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M7.5 7.5h-.75A2.25 2.25 0 0 0 4.5 9.75v7.5a2.25 2.25 0 0 0 2.25 2.25h7.5a2.25 2.25 0 0 0 2.25-2.25v-7.5a2.25 2.25 0 0 0-2.25-2.25h-.75m-6 3.75 3 3m0 0 3-3m-3 3V1.5m6 9h.75a2.25 2.25 0 0 1 2.25 2.25v7.5a2.25 2.25 0 0 1-2.25 2.25h-7.5a2.25 2.25 0 0 1-2.25-2.25v-.75"
-            />
-          </svg>
         </div>
       </div>
-      <section class="max-w-[700] mx-auto px-7 py-3 border-b border-white"></section>
+      <div>
+        <section className="max-w-[700px] mx-auto px-7 py-3 border-b border-white"></section>
+        <div className="flex flex-col sm:flex-row justify-between items-center">
+          <div className="text-left pt-2">
+            <p>craig-moore22@outlook.com</p>
+          </div>
+          <div className="text-right pt-2"></div>
+        </div>
+      </div>
     </section>
   );
 }
